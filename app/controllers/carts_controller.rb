80b5ccc5c7414cb7c1c6bb42413bd085b77be431
railsdreamteam <ninjarendan@gmail.com>
2020-04-06 18:01:05 +0300

    06.04.2020

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -6,23 +6,30 @@ class CartsController < ApplicationController
   ]
 
   def show
-    @cart = Cart.find(params[:id]) if params[:id]
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def product_to_cart
     @product = Product.find(params[:product_id])
-    product_in_cart = @cart.cart_products.find_or_create_by(product_id: @product.id)
-    if params[:product_count]
-      product_in_cart.update(product_count: params[:product_count])
-    else
-      if product_in_cart.product_count == nil or product_in_cart.product_count < @product.quantity
-        product_in_cart.increment!(:product_count)
-      end
-      end
-    product_in_cart.update(total_product_sum: product_in_cart.product_count * product_in_cart.product.price)
+    @product_in_cart = @cart.cart_products.find_or_create_by(product_id: @product.id)
+    @add_product_count = params[:product_count]&.to_i || 1
+    @add_in_cart_modal = params[:add_in_cart_modal]
+
     respond_to do |format|
-      format.js
+      if can_add_product_into_cart?
+        @product_in_cart.update(product_count: @add_product_count) if params[:product_count]
+        @product_in_cart.increment!(:product_count, @add_product_count) unless params[:product_count]
+        @total_product_sum = @product_in_cart.product_count * @product_in_cart.product.price
+        @product_in_cart.update(total_product_sum: @total_product_sum)
+        format.js
+      else
+        format.js {render :qwe}
+      end
     end
+
   end
 
   def delete_product_from_cart
@@ -41,6 +48,11 @@ class CartsController < ApplicationController
   end
 
   private
+    def can_add_product_into_cart?
+      params[:product_count] && @add_product_count <= @product.quantity ||
+      @add_product_count + (@product_in_cart.product_count || 0) <= @product.quantity
+    end
+
     def set_cart_for_add_product
       if session[:current_cart_id]
         @cart = Cart.find_by_id(session[:current_cart_id])
