88b6991af8c959250428ed5b332192bbbf287be0
railsdreamteam <ninjarendan@gmail.com>
2020-04-22 00:41:45 +0300

    edit_product_edit

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -43,7 +43,7 @@ class Admin::ProductsController < ApplicationController
   def update
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to edit_admin_product_path(@product), notice: 'Product was successfully updated.' }
+        format.html { redirect_to admin_products_path(@product), notice: 'Product was successfully updated.' }
         format.json { render :show, status: :ok, location: @product }
       else
         format.html { render :edit }
