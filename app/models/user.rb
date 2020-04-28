e1d2aa377dd0dd04d4de7e796f64bf475be6c9c6
railsdreamteam <ninjarendan@gmail.com>
2020-04-28 17:10:13 +0300

    add mailgun

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable, and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable, :validatable and :omniauthable
   has_many :orders, dependent: :nullify
   has_many :product_comments, dependent: :destroy
 
@@ -35,6 +35,5 @@ class User < ApplicationRecord
   devise :database_authenticatable,
          :registerable,
          :recoverable,
-         :rememberable,
-         :validatable
+         :rememberable
 end
