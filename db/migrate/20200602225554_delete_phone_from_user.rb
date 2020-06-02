38f3a8ed422fc4b76f87bdee960642540e84ea0b
railsdreamteam <ninjarendan@gmail.com>
2020-06-03 01:57:18 +0300

    02.05.2020

---
+++ db/migrate/20200602225554_delete_phone_from_user.rb
@@ -0,0 +1,5 @@
+class DeletePhoneFromUser < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :users, :phone
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_02_130436) do
+ActiveRecord::Schema.define(version: 2020_06_02_225554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -194,7 +194,6 @@ ActiveRecord::Schema.define(version: 2020_06_02_130436) do
     t.string "last_name"
     t.string "city"
     t.string "departament"
-    t.string "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
